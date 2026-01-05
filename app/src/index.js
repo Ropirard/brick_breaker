940d691f85d228e04e5bc63b9683c031622dd929
ES_PREPA_DEV <pirardrobin745@gmail.com>
2026-01-05 16:57:42 +0100

    Add canvas + semi pacman

--- app/src/index.js
+++ app/src/index.js
@@ -1,4 +1,21 @@
 // Import de la feuille de style
 import './assets/css/style.css';
 
-console.log( 'Allez, au boulot ! 🚀' );
+const elH1 = document.createElement('h1');
+elH1.textContent = 'Yo, Angelo';
+
+document.body.append(elH1);
+
+const elCanvas = document.createElement('canvas');
+elCanvas.width = 800;
+elCanvas.height = 600;
+
+document.body.append(elCanvas);
+
+// Le contexte de dessin
+const ctx = elCanvas.getContext('2d');
+ctx.beginPath();
+ctx.fillStyle = '#fc0';
+ctx.arc(400,300,100,Math.PI/6,-Math.PI/6);
+ctx.closePath();
+ctx.fill();
