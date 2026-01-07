181e9ff85a6e16a610cffd998431393f3695a22f
ES_PREPA_DEV <pirardrobin745@gmail.com>
2026-01-07 16:54:19 +0100

    Add collisions, be able to modify ball speed and angle

---
+++ app/src/Game/CustomMath.js
@@ -0,0 +1,7 @@
+export default class CustomMath
+{
+    // Conversion d'angles Degrés => Radians
+    static degToRad(degValue) {
+        return degValue * (Math.PI / 180);
+    }
+}
--- app/src/Game/Game.js
+++ app/src/Game/Game.js
@@ -1,19 +1,33 @@
 // Import de la feuille de style
 import '../assets/css/style.css'
 import ballImgSrc from '../assets/img/ball.png';
+import CustomMath from './CustomMath';
 class Game {
 
     //context du dessin du canvas
     ctx;
+    // Images 
+    ballImg;
 
     // Temporaire : position de base de la balle
     ballX = 400;
     ballY = 300;
+    ballSpeed = 8;
+    ballAngle = 30;
+    ballVelocity = {
+        x: this.ballSpeed * Math.cos(CustomMath.degToRad(this.ballAngle)), // Trajectoire de la balle avec 30° d'angle (PI/6)
+        y: this.ballSpeed * -1 * Math.sin(CustomMath.degToRad(this.ballAngle))
+    };
 
     start() {
         console.log('jeu démarré');
+        // initialisation de l'interface HTML
         this.initHtmlUI();
+        // Initialisation des objets du jeu
+        this.initGameObjects();
+        // Lancement de la boucle
         requestAnimationFrame(this.loop.bind(this));
+        // Après la boucle 
     }
 
     // Méthode 'privée'
@@ -34,19 +48,40 @@ class Game {
 
     }
 
+    // Mise en place des objets du jeu dans la scene
+    initGameObjects() {
+        //1- On créer une baluise HTML <img> qui sera jamais ajoutée au DOM
+        this.ballImg = new Image();
+        //2- on récuère le nom de l'image génére par webpack
+        this.ballImg.src = ballImgSrc;
+        //3- On demande au context de dessiner cette image ds le canvas
+        this.ctx.drawImage(this.ballImg, this.ballX, this.ballY);
+    }
+
     // Boucle d'animation
     loop() {
-        //1- On créer une baluise HTML <img> qui sera jamais ajoutée au DOM
-        const ballImg = new Image();
-        //2- on récuère le nom de l'image génére par webpack
-        ballImg.src = ballImgSrc;
-        //3- on dmd au context de dessiner cette image ds le canvas
-        ballImg.addEventListener('load', () => {
-            this.ctx.drawImage(ballImg, this.ballX, this.ballY);         
-        });
         // Mise à jour de la position de la balle
-        this.ballX ++;
-        this.ballY --;
+        this.ballX += this.ballVelocity.x;
+        this.ballY += this.ballVelocity.y;
+
+        // TODO : Détection des collisions 
+        // Collision avec le coté droit de la scène : Inversion du X de la vélocité
+        if( this.ballX + 20 >= 800 || this.ballX <= 0 ) {
+            this.ballVelocity.x *= -1;
+        }
+
+        // Collision avec le coté bas ou haut de la scène : Inversion du Y de la vélocité
+        if( this.ballY + 20 >= 600 || this.ballY <= 0 ) {
+            this.ballVelocity.y *= -1;
+        }
+
+        // == RENDU VISUEL == //
+
+        // On efface tous les dessins
+        this.ctx.clearRect(0, 0, 800, 600);
+
+        // Dessin des objets
+        this.ctx.drawImage(this.ballImg, this.ballX, this.ballY);
 
         // Appel de la frame suivante 
         requestAnimationFrame(this.loop.bind(this));
